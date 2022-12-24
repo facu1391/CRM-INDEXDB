75d388e2f639be1fbdf22a44d1d3a387e13eda96
Facundo Nuñez <105162692+facu1391@users.noreply.github.com>
2022-12-24 18:35:46 -0300

    Add files via upload

---
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -0,0 +1,72 @@
+(function() {
+    let DB;
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        conectarDB();
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+    
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        //Leer todos los inputs
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        if(nombre === '' || email === '' || telefono === '' || empresa === '') {
+            imprirmirAlerta('Todos los campos son obligatorios', 'error');
+            return;
+        }
+
+        //Crear un objeto con la informacion
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+
+        cliente.id = Date.now();
+
+        crearNuevoCliente(cliente);
+    }
+
+    function conectarDB() {
+        const abrirConexion = window.indexedDB.open('crm', 1);
+    
+        abrirConexion.onerror = function() {
+            console.log('Hubo un error');
+        };
+    
+        abrirConexion.onsuccess = function() {
+            DB = abrirConexion.result;
+        }
+    }
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            imprirmirAlerta('Hubo un error', 'error');
+        }
+
+        transaction.oncomplete = function() {
+            console.log('Cliente agregado');
+
+            imprirmirAlerta('El cliente se agregado correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+    }
+
+})();
